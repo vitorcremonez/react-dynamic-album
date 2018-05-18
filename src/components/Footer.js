e94809c76e5f409d38b38271bfc3bb619cbdb33f
Vitor Cremonez <vitorcremonez@gmail.com>
2018-05-18 17:13:32 -0300

    added Modal of album creator.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const Footer = () => {
-    return (
-        <p>Lorem Ipsum</p>
-    );
+import $ from 'jquery';
+import ModalAlbumCreator from "./ModalAlbumCreator";
+
+class Footer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            modal: false
+        };
+        this.toggle = this.toggle.bind(this);
+    }
+
+    toggle() {
+        this.setState({
+            modal: !this.state.modal
+        });
+    }
+
+    render() {
+        return (
+            <div className="text-black-50 p-5">
+                <div className="font-weight-bold">
+                    Dynamic Album!
+                </div>
+                <div>
+                    <a href={'#'} onClick={() => this.toggle()}>
+                        Create my dynamic album too!
+                    </a>
+                    <ModalAlbumCreator isOpen={this.state.modal} toggle={this.toggle}/>
+                </div>
+                <div>
+                    Created by Vitor Cremonez <a href="https://github.com/vitorcremonez/react-dynamic-album">github</a>
+                </div>
+            </div>
+        );
+    }
+
 };
 
 export default Footer;
---
+++ src/components/ModalAlbumCreator.js
@@ -0,0 +1,146 @@
+import React, {Component} from 'react';
+import {
+    Modal,
+    ModalHeader,
+    ModalBody,
+    Button,
+    Form,
+    FormGroup,
+    Label,
+    Input,
+} from 'reactstrap';
+import $ from "jquery";
+
+class ModalAlbumCreator extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: "",
+            description: "",
+            images: [],
+        };
+    }
+
+    renderLink() {
+        const obj = {
+            title: this.state.title,
+            description: this.state.description,
+            images: this.state.images,
+        };
+        const recursiveEncoded = $.param(obj);
+        const recursiveDecoded = decodeURIComponent($.param(obj));
+
+
+        return (
+            <a href={`http://localhost:3000?${recursiveEncoded}`} target={'_blank'}>
+                {`http://localhost:3000?${recursiveEncoded}`}
+            </a>
+        );
+    }
+
+    renderImagesForm() {
+        return this.state.images.map((image, index) => {
+            return (
+                <div>
+                    <Label className="font-weight-bold">Image #{index + 1}:</Label>
+                    <Input
+                        type="text"
+                        placeholder="Description..."
+                        className="mb-2"
+                        value={this.state.images[index].description}
+                        onChange={(event) => {
+                            const images = this.state.images;
+                            images[index].description = event.target.value;
+                            this.setState({images: images});
+                        }}
+                    />
+                    <Input
+                        type="text"
+                        placeholder="https://examplehost.com/your-image.png"
+                        className="mb-2"
+                        value={this.state.images[index].url}
+                        onChange={(event) => {
+                            const images = this.state.images;
+                            images[index].url = event.target.value;
+                            this.setState({images: images});
+                        }}
+                    />
+                </div>
+            );
+        })
+    }
+
+    renderForm() {
+        return (
+            <Form>
+                <FormGroup>
+                    <Label className="font-weight-bold">Title:</Label>
+                    <Input
+                        type="text"
+                        name="name"
+                        placeholder="Album title"
+                        value={this.state.title}
+                        onChange={(event) => this.setState({title: event.target.value})}
+                    />
+                </FormGroup>
+
+                <FormGroup>
+                    <Label className="font-weight-bold">Description:</Label>
+                    <Input
+                        type="textarea"
+                        name="text"
+                        placeholder="Album description"
+                        value={this.state.description}
+                        onChange={(event) => this.setState({description: event.target.value})}
+                    />
+                </FormGroup>
+
+                <FormGroup>
+                    <FormGroup>
+                        <div className="p-2 rounded" style={{backgroundColor: '#EEE'}}>
+                            { this.renderImagesForm() }
+
+                            <div className="text-center">
+                                <Button type="button" color="danger" onClick={() => {
+                                    this.setState({
+                                        images: [
+                                            ...this.state.images,
+                                            {
+                                                description: "",
+                                                url: "",
+                                            }
+                                        ]
+                                    });
+                                }}>
+                                    + Image
+                                </Button>
+                            </div>
+                        </div>
+                    </FormGroup>
+                </FormGroup>
+
+                <div className="text-center p-2 responsive-text"
+                     style={{borderWidth: 2, borderColor: '#EEE', borderStyle: 'dashed', borderRadius: 5}}>
+                    {this.renderLink()}
+                </div>
+            </Form>
+        );
+    }
+
+
+    render() {
+        console.log(this.state);
+        return (
+            <Modal isOpen={this.props.isOpen} toggle={() => this.props.toggle()}>
+                <ModalHeader>
+                    Dynamic Album!
+                </ModalHeader>
+                <ModalBody>
+                    {this.renderForm()}
+                </ModalBody>
+            </Modal>
+        );
+    }
+}
+
+export default ModalAlbumCreator;
